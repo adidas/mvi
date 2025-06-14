221840eba434f3c893d7ee4d109b58ad6ee85d4d
extmkv <github@jcosta.pt>
2025-06-15 00:02:32 +0200

    Make sure intent can only be called inside MviHost

--- mvi/src/commonMain/kotlin/com/adidas/mvi/dsl/IntentDslScope.kt
+++ mvi/src/commonMain/kotlin/com/adidas/mvi/dsl/IntentDslScope.kt
@@ -9,31 +9,30 @@ import com.adidas.mvi.transform.StateTransform
 import com.adidas.mvi.transform.ViewTransform
 import kotlinx.coroutines.flow.FlowCollector
 
-public class IntentDslScope<StateT : LoggableState, SideEffectT : Any>(
-    public val flowCollector: FlowCollector<StateTransform<State<StateT, SideEffectT>>>,
+public class IntentDslScope<ViewT : LoggableState, SideEffectT : Any>(
+    public val flowCollector: FlowCollector<StateTransform<State<ViewT, SideEffectT>>>,
 ) {
-    public suspend fun reduce(block: (StateT) -> StateT) {
-        val transform =
-            object : ViewTransform<StateT, SideEffectT>() {
-                override fun mutate(currentState: StateT): StateT = block(currentState)
-            }
+    public suspend fun reduce(block: (ViewT) -> ViewT) {
+        val transform = object : ViewTransform<ViewT, SideEffectT>() {
+            override fun mutate(currentState: ViewT): ViewT = block(currentState)
+        }
         flowCollector.emit(transform)
     }
 
     public suspend fun postSideEffect(effect: SideEffectT) {
         val transform =
-            object : SideEffectTransform<StateT, SideEffectT>() {
+            object : SideEffectTransform<ViewT, SideEffectT>() {
                 override fun mutate(sideEffects: SideEffects<SideEffectT>): SideEffects<SideEffectT> = sideEffects.add(effect)
             }
 
         flowCollector.emit(transform)
     }
 
-    public suspend inline fun <reified SpecificStateT : StateT> reduceInState(noinline reducer: (SpecificStateT) -> StateT) {
+    public suspend inline fun <reified SpecificViewT : ViewT> reduceInState(noinline reducer: (SpecificViewT) -> ViewT) {
         val transform =
-            object : ViewTransform<StateT, SideEffectT>() {
-                override fun mutate(currentState: StateT): StateT {
-                    return requireAndReduceState<StateT, SpecificStateT>(currentState) {
+            object : ViewTransform<ViewT, SideEffectT>() {
+                override fun mutate(currentState: ViewT): ViewT {
+                    return requireAndReduceState<ViewT, SpecificViewT>(currentState) {
                         reducer(it)
                     }
                 }
--- mvi/src/commonMain/kotlin/com/adidas/mvi/dsl/SyntaxDsl.kt
+++ mvi/src/commonMain/kotlin/com/adidas/mvi/dsl/SyntaxDsl.kt
@@ -1,12 +1,14 @@
 package com.adidas.mvi.dsl
 
+import com.adidas.mvi.Intent
 import com.adidas.mvi.LoggableState
+import com.adidas.mvi.MviHost
 import com.adidas.mvi.State
 import com.adidas.mvi.transform.StateTransform
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
-public fun <StateT : LoggableState, SideEffectT : Any> intent(block: suspend IntentDslScope<StateT, SideEffectT>.() -> Unit): Flow<StateTransform<State<StateT, SideEffectT>>> =
+public fun <ViewT : LoggableState, SideEffectT : Any, IntentT : Intent> MviHost<IntentT, State<ViewT, SideEffectT>>.intent(block: suspend IntentDslScope<ViewT, SideEffectT>.() -> Unit): Flow<StateTransform<State<ViewT, SideEffectT>>> =
     flow {
         val scope = IntentDslScope(this)
         scope.block()
