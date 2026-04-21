91fc0cc4ba8f3abf5697cef9c51f1ed04e9eaa5c
Matej Drobnic <matej.drobnic@inova.si>
2026-04-21 07:35:18 +0200

    Add awaitView extension

--- mvi/src/jvmTest/kotlin/com/adidas/mvi/reducer/ReducerExtensionsTest.kt
+++ mvi/src/jvmTest/kotlin/com/adidas/mvi/reducer/ReducerExtensionsTest.kt
@@ -2,26 +2,70 @@ package com.adidas.mvi.reducer
 
 import com.adidas.mvi.Intent
 import com.adidas.mvi.State
+import com.adidas.mvi.product.FakeProductViewTransform
+import com.adidas.mvi.product.ProductSideEffect
 import com.adidas.mvi.product.ProductState
+import com.adidas.mvi.sideeffects.SideEffects
 import com.adidas.mvi.transform.StateTransform
 import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.engine.coroutines.backgroundScope
+import io.kotest.engine.coroutines.testScheduler
 import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.yield
 
-class ReducerExtensionsTest : ShouldSpec({
-    context("A reducer instantiated with the extension") {
-        val reducer =
-            Reducer(
-                coroutineScope = TestScope(),
-                initialInnerState = ProductState.Loading,
-                intentExecutor = { _: Intent ->
-                    emptyFlow<StateTransform<State<ProductState, Unit>>>()
-                },
-            )
+class ReducerExtensionsTest : ShouldSpec(
+    {
+        coroutineTestScope = true
 
-        should("The initial inner state should be Loading") {
-            reducer.state.value.view shouldBe ProductState.Loading
+        context("A reducer instantiated with the extension") {
+            val reducer =
+                Reducer(
+                    coroutineScope = TestScope(),
+                    initialInnerState = ProductState.Loading,
+                    intentExecutor = { _: Intent ->
+                        emptyFlow<StateTransform<State<ProductState, Unit>>>()
+                    },
+                )
+
+            should("The initial inner state should be Loading") {
+                reducer.state.value.view shouldBe ProductState.Loading
+            }
         }
-    }
-})
+
+        context("A reducer instantiated ") {
+            val reducer =
+                Reducer<Intent, ProductState, ProductSideEffect>(
+                    coroutineScope = backgroundScope,
+                    initialInnerState = ProductState.Loading,
+                    intentExecutor = { intent: Intent ->
+                        println("got intent $intent")
+                        if (intent is TestIntent.CainIntent) {
+                            flowOf<StateTransform<State<ProductState, ProductSideEffect>>>(FakeProductViewTransform(State(ProductState.Loaded, SideEffects())))
+                        } else {
+                            emptyFlow<StateTransform<State<ProductState, ProductSideEffect>>>()
+                        }
+                    },
+                )
+
+            val awaitJob = backgroundScope.async { reducer.awaitView<ProductState.Loaded, _>() }
+            testScheduler.runCurrent()
+
+            should("The initial await state should not return yet") {
+                awaitJob.isCompleted shouldBe false
+            }
+
+
+            should("return after emitting Loaded") {
+                reducer.executeIntent(TestIntent.CainIntent)
+                testScheduler.runCurrent()
+
+                awaitJob.isCompleted shouldBe true
+                awaitJob.await() shouldBe ProductState.Loaded
+            }
+        }
+    },
+)
